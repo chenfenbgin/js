06a88f9abcecb06783a4ba2bcd8ad5645cb5cc22
chenfengbin <1838272147@qq.com>
2022-05-22 22:53:50 +0800

    手写 promise

---
+++ 18_Promise/15.手写Promise-结构的设计.js
@@ -0,0 +1,40 @@
+// 1.状态的设计
+const PROMISE_STATE_PENDING = "pending";
+const PROMISE_STATE_FULFILED = "fulfiled";
+const PROMISE_STATE_REJECTED = "rejected";
+
+class MyPromise {
+  constructor(executor) {
+    this.status = PROMISE_STATE_PENDING;
+    this.value = null;
+    this.reason = null;
+
+    const resolve = (value) => {
+      if (this.status === PROMISE_STATE_PENDING) {
+        this.status = PROMISE_STATE_FULFILED;
+        this.value = value;
+        console.log("resolve被调用了");
+      }
+    };
+    const reject = (reason) => {
+      if (this.status === PROMISE_STATE_PENDING) {
+        this.status = PROMISE_STATE_REJECTED;
+      }
+      console.log("reject被调用了");
+    };
+    executor(resolve, reject);
+  }
+}
+
+const promise = new MyPromise((resolve, reject) => {
+  console.log("传入的函数直接被调用了 pending===");
+
+  reject(111);
+  resolve(222);
+});
+
+// 当我们调用then的时候，应该需要两个变量来保存
+promise.then(
+  (res) => {},
+  (err) => {}
+);
---
+++ 18_Promise/16.手写Promise-then方法的设计.js
@@ -0,0 +1,58 @@
+// 1.状态的设计
+const PROMISE_STATE_PENDING = "pending";
+const PROMISE_STATE_FULFILED = "fulfiled";
+const PROMISE_STATE_REJECTED = "rejected";
+
+class MyPromise {
+  constructor(executor) {
+    this.status = PROMISE_STATE_PENDING;
+    this.value = null;
+    this.reason = null;
+
+    const resolve = (value) => {
+      if (this.status === PROMISE_STATE_PENDING) {
+        this.status = PROMISE_STATE_FULFILED;
+        queueMicrotask(() => {
+          this.value = value;
+          console.log("resolve被调用了");
+          // 我们需要执行then传进来的第一个函数
+          this.onfulfilled(this.value);
+        });
+      }
+    };
+    const reject = (reason) => {
+      if (this.status === PROMISE_STATE_PENDING) {
+        this.status = PROMISE_STATE_REJECTED;
+        queueMicrotask(() => {
+          this.reason = reason;
+          console.log("reject被调用了");
+          // 我们需要执行then传进来的第二个函数
+          this.onrejected(this.reason);
+        });
+      }
+    };
+    executor(resolve, reject);
+  }
+
+  then(onfulfilled, onrejected) {
+    this.onfulfilled = onfulfilled;
+    this.onrejected = onrejected;
+  }
+}
+
+const promise = new MyPromise((resolve, reject) => {
+  console.log("传入的函数直接被调用了 pending===");
+
+  reject(111);
+  resolve(222);
+});
+
+// 当我们调用then的时候，应该需要两个变量来保存
+promise.then(
+  (res) => {
+    console.log("res:", res);
+  },
+  (err) => {
+    console.log("err", err);
+  }
+);
